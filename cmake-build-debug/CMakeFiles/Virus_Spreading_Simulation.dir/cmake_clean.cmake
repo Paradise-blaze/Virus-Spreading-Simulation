f2b0d4dc1672e4dd06db795e0f2b9d0da0c0e21f
ProxPxD <piotr10tutek@poczta.onet.pl>
2020-04-24 21:06:22 +0200

    project skeleton

---
+++ cmake-build-debug/CMakeFiles/Virus_Spreading_Simulation.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Virus_Spreading_Simulation.dir/main.cpp.o"
+  "Virus_Spreading_Simulation"
+  "Virus_Spreading_Simulation.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Virus_Spreading_Simulation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
