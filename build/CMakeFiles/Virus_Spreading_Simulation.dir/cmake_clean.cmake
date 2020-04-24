34a612b446950eeb09cf2842afa57b6c423d3e84
Jasiek <jan4000@o2.pl>
2020-04-24 22:24:03 +0200

    VSCode CMake

---
+++ build/CMakeFiles/Virus_Spreading_Simulation.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Virus_Spreading_Simulation.dir/Region.cpp.obj"
+  "CMakeFiles/Virus_Spreading_Simulation.dir/main.cpp.obj"
+  "Virus_Spreading_Simulation.exe"
+  "Virus_Spreading_Simulation.exe.manifest"
+  "Virus_Spreading_Simulation.pdb"
+  "libVirus_Spreading_Simulation.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Virus_Spreading_Simulation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
