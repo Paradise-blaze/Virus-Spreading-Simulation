252fca96c4ae6dd5ce12661fc8681a01060caf39
ProxPxD <piotr10tutek@poczta.onet.pl>
2020-05-30 17:55:18 +0200

    Pure window

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
+import pygame
+
+pygame.init()
+
+height = 576
+width  = 1024
+
+screen = pygame.display.set_mode((width, height) )
 
 
-def main():
-    print("Hello world!")
+pygame.display.set_caption("NanoWar")
+
+clock = pygame.time.Clock()
+done = False
+
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+    #gameplay.play(screen)
+
+    clock.tick(60)
+    pygame.display.flip()
+
+
 
 
 if __name__ == '__main__':
