eb27010b81490becc7393177f1934160495abc91
ProxPxD <piotr10tutek@poczta.onet.pl>
2020-05-31 19:43:14 +0200

    Changable menu feature

--- Window.py
+++ Window.py
@@ -4,18 +4,37 @@ from PIL import Image, ImageTk
 import time
 
 
-def realize(master, element):
-    lf = tk.LabelFrame()
-    print(element.tag, element.attrib)
-    for sub in element:
-        realize(master, sub)
-    return lf
-
-
 class Window:
 
-    def __init__(self, title):
-        self.root = tk.Tk()
+    def realize(self, root, to_add):
+        if to_add.tag == "Form":
+            for sub in to_add:
+                self.realize(root, sub)
+        else:
+            tag = to_add.tag
+            constructor = getattr(tk, tag, None)
+            if constructor:
+                options = to_add.attrib.copy()
+                print(tag, options)
+                elem = constructor(root, **options)
+                if 'command' in options:
+                    func = getattr(Window, options['command'], lambda _: None)
+                    if func:
+                        elem.config(command=lambda: func(self))
+                elif tag == "Button":
+                    elem.config(command= lambda: self.change_menu(elem))
+                for sub in to_add:
+                    self.realize(elem, sub)
+                elem.pack()
+
+        return root
+
+    def __init__(self, title, xml):
+        self.root = self.realize(tk.Tk(), et.parse(xml).getroot())
+        self.panel = self.root.nametowidget("content.background")
+        self.menu = self.root.nametowidget("content.menu")
+        self.menus = {}
+        self.set_menus()
         self.title = title
         self.width = int(720 * 1.618)
         self.height = 720
@@ -40,17 +59,28 @@ class Window:
         self.width = width
         self.height = height
 
+    def set_menus(self):
+        menu_holder = self.root.nametowidget("menus")
+        for menu in menu_holder.winfo_children():
+            menu.pack_forget() #hides menu
+            self.menus[menu.winfo_name()] = menu
+
     def open(self):
         self.root.title(self.title)
         self.load()
         self.scale()
-        panel = tk.Label(self.root, image=self.scaled["background"])
-        panel.pack(side="bottom", fill="both", expand="yes")
-        self.root.resizable(width=False, height=False)
+        self.panel.config(image=self.scaled["background"])
+        self.panel.pack(side="bottom", fill="both", expand="yes")
+
+        self.menu = self.menus['initial']
+        self.menu.pack()
+        self.root.config(menu=self.menu)
+        #self.root.resizable(width=False, height=False)
         self.refresh()
         self.root.mainloop()
 
     def load(self):
+        #load images
         for name in ["background", "map"]:
             self.images[name] = Image.open(self.paths[name])
 
@@ -59,7 +89,10 @@ class Window:
             image = self.images[name].resize((self.width, self.height), Image.ANTIALIAS)
             self.scaled[name] = ImageTk.PhotoImage(image)
 
-    def refresh(self):
-        print(time.asctime())
-        self.root.after(1000, self.refresh) #refreshes every second
+    def change_menu(self, elem):
+        self.menu.pack_forget()
+        self.menu = self.menus[elem.winfo_name()]
+        self.menu.pack()
 
+    def refresh(self):
+        self.root.after(1000, self.refresh) #refreshes every second
--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from Window import Window
 
 
-window = Window("Virus Spreading")
+window = Window("Virus Spreading", "gui.xml")
 window.set_background_path("background.png")
 window.set_map_path("map.png")
 size = 580
 window.set_dimensions(int(size * 1.618), size)
 window.open()
-
+'''
+<LabelFrame text='Hello World' borderwidth='2' relief='groove' column='0' row='0' columnspan='2'>
+        <Label text='Entry:' column='0' row='0'/>
+        <Checkbutton name='check' text='checkbutton' column='1' row='0'/>
+        <Entry name='entry' bg='gold' width='30' column='0' row='1' columnspan='2'/>
+    </LabelFrame>
+    <Button name='ok' text='OK' column='0' row='1'/>
+    <Button name='cancel' text='Cancel' column='1' row='1'/>
+'''
 '''
 img = ImageTk.PhotoImage(Image.open("test.png"))
         panel = tk.Label(root, image=img)
