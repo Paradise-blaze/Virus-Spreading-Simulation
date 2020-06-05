61ef13c4b8eab2618a935d82a97531064fde4b6f
ProxPxD <piotr10tutek@poczta.onet.pl>
2020-06-05 19:37:53 +0200

    Fixing null button command and extending menu

--- Window.py
+++ Window.py
@@ -15,10 +15,10 @@ class Window:
             constructor = getattr(tk, tag, None)
             if constructor:
                 options = to_add.attrib.copy()
-                print(tag, options)
+                # print(tag, options)
                 elem = constructor(root, **options)
-                if 'command' in options:
-                    func = getattr(Window, options['command'], lambda _: None)
+                if 'command' in options:  # Empty function will be executed if no viable name has been typed
+                    func = getattr(Window, options['command'], lambda _1, _2: None)
                     if func:
                         elem.config(command=lambda: func(self, elem))
                 elif tag == "Button":
@@ -31,14 +31,12 @@ class Window:
 
     def __init__(self, title, xml):
         self.root = self.realize(tk.Tk(), et.parse(xml).getroot())
+        self.root.title(title)
         self.panel = self.root.nametowidget("content.background")
         self.menu_pack_options = {}
         self.menu_children_config_options = {}
-        self.set_options()
-        self.menu = self.root.nametowidget("content.menu")
+        self.menu = None
         self.menus = {}
-        self.load_menus()
-        self.title = title
         self.width = int(720 * 1.618)
         self.height = 720
         self.img = None
@@ -47,7 +45,7 @@ class Window:
         self.scaled = {}
 
     def set_title(self, title):
-        self.title = title
+        self.root.title(title)
 
     def set_data_path(self, path):
         self.paths["data"] = path
@@ -62,13 +60,15 @@ class Window:
         self.width = width
         self.height = height
 
-    def set_options(self):
-        #pack options
-        self.menu_pack_options['side'] = tk.LEFT #horizontal alignment
-        #configurations
-        #self.menu_children_config_options[''] =
+    def load_menu_options(self):
+        # pack options
+        self.menu_pack_options['side'] = tk.LEFT  # horizontal alignment
+        # configurations
+        # self.menu_children_config_options[''] =
+        pass
 
-    def set_configuration(self, original, copy):
+    @staticmethod
+    def copy_configuration(original, copy):
         for config in original.config():
             try:
                 copy.config({config: original.cget(config)})
@@ -78,52 +78,57 @@ class Window:
     def load_menus(self):
         menu_holder = self.root.nametowidget("menus")
         for menu in menu_holder.winfo_children():
-            self.set_configuration(self.menu, menu)
+            self.copy_configuration(self.menu, menu)
             for child in menu.winfo_children():
                 child.pack(**self.menu_pack_options)
-            menu.pack_forget() #hides menu
+            menu.pack_forget()  # hides menu
             self.menus[menu.winfo_name()] = menu
 
-    def open(self):
-        self.root.title(self.title)
-        self.load()
-        self.scale()
+    def load_images(self):
+        for name in ["background", "map"]:
+            self.images[name] = Image.open(self.paths[name])
+            self.scaled[name] = self.scale_image(self.images[name])
+
+    def scale_image(self, image):
+        scaled = image.resize((self.width, self.height), Image.ANTIALIAS)
+        return ImageTk.PhotoImage(scaled)
+
+    def load_all(self):
+        self.load_images()
+        self.load_menu_options()
+        self.menu = self.root.nametowidget("content.menu")
+        self.load_menus()
+        self.menu = self.menus['initial']
+    
+    def set_all(self):
         self.panel.config(image=self.scaled["background"])
         self.panel.pack(side="bottom", fill="both", expand="yes")
-
-        self.menu = self.menus['initial']
         self.menu.pack()
         self.root.config(menu=self.menu)
-        #self.root.resizable(width=False, height=False)
+        # self.root.resizable(width=False, height=False)
+
+    def open(self):
+        self.load_all()
+        self.set_all()
+
         self.refresh()
         self.root.mainloop()
 
-    def load(self):
-        #load images
-        for name in ["background", "map"]:
-            self.images[name] = Image.open(self.paths[name])
-
-    def scale(self):
-        for name in ["background", "map"]:
-            image = self.images[name].resize((self.width, self.height), Image.ANTIALIAS)
-            self.scaled[name] = ImageTk.PhotoImage(image)
-
-    def change_menu(self, elem):
+    def change_menu(self, arg):
         self.menu.pack_forget()
-        name = elem.winfo_name()
+        name = arg if isinstance(arg, str) else arg.winfo_name()
         if name in self.menus:
             self.menu = self.menus[name]
 
         self.menu.pack()
 
     def refresh(self):
-        self.root.after(1000, self.refresh) #refreshes every second
+        self.root.after(1000, self.refresh)  # refreshes every second
 
     # button methods
-
-    def start_button(self, elem):
+    def start_button(self, widget):
         self.panel.config(image=self.scaled["map"])
-        self.change_menu(elem)
+        self.change_menu(widget)
 
     def close_window(self, _):
         self.root.quit()
