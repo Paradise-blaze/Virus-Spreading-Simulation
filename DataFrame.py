5d142393dc5237db049b1a887e9f2e3c7f7759af
Szymon Auguścik <szyauguscik@gmail.com>
2020-06-08 17:26:55 +0200

    Code refactored. Method to gather world data applied

--- DataFrame.py
+++ DataFrame.py
@@ -4,6 +4,44 @@ import pandas as pd
 import os
 
 
+class MapGenerator:
+    def __init__(self, path):
+        self.geopandas_countries = gp.read_file(gp.datasets.get_path('naturalearth_lowres'))
+        self.csv_countries = pd.read_csv(path, header=0, sep=';')
+
+    def clear_data(self):
+        to_drop = ['Antarctica', 'Falkland Is.', 'Fr. S. Antarctic Lands', 'N. Cyprus', 'Palestine', 'Taiwan',
+                   'eSwatini', 'W. Sahara']
+
+        for i in to_drop:
+            self.csv_countries = self.csv_countries[self.csv_countries.name != i]
+
+    def generate_maps(self, group, frame_list):
+        step = 0
+        pandemic_condition = {}  # country/bool pair
+        data_to_map = {'name': [], group: []}
+
+        for frame in frame_list:
+            data_to_map['name'].append(frame.name)
+            data_to_map[group].append(0)
+            pandemic_condition[frame.name] = True
+
+        dead_in_countries = pd.DataFrame(data_to_map, columns=['name', group])
+        dead_in_countries = dead_in_countries.set_index('name')
+        merged = merge_data_frames(self.geopandas_countries, self.csv_countries, dead_in_countries)
+
+        while True in pandemic_condition.values():
+            for frame in frame_list:
+                actual_list = frame.choose_list(group)
+                if step in range(frame.start_day, frame.end_day + 1):
+                    dead_in_countries.loc[frame.name][group] = actual_list[step - frame.start_day]
+                    merged = merge_data_frames(self.geopandas_countries, self.csv_countries, dead_in_countries)
+                elif step > frame.end_day:
+                    pandemic_condition[frame.name] = False
+            draw_map(merged, step, group)
+            step += 1
+
+
 class DataFrame:
     def __init__(self, path):
         file = open(path)
@@ -13,21 +51,31 @@ class DataFrame:
         self.start_day = int(lines[0].split(';')[0])
         self.end_day = int(lines[len(lines)-1].split(';')[0])
 
-        self.dead_list = []
+        self.susceptible = []
+        self.exposed = []
+        self.infectious = []
+        self.recovered = []
+        self.dead = []
 
         for line in lines:
             numbers = line.split(';')
-            self.dead_list.append(int(numbers[(len(numbers)-1)].strip()))
+            self.susceptible.append(int(numbers[1]))
+            self.exposed.append(int(numbers[2]))
+            self.infectious.append(int(numbers[3]))
+            self.recovered.append(int(numbers[4]))
+            self.dead.append(int(numbers[5].strip()))
 
-
-def clear_data(gp_data):
-    to_drop = ['Antarctica', 'Falkland Is.', 'Fr. S. Antarctic Lands', 'N. Cyprus', 'Palestine', 'Taiwan', 'eSwatini',
-              'W. Sahara']
-
-    for i in to_drop:
-        gp_data = gp_data[gp_data.name != i]
-
-    return gp_data.sort_values(by=['name'])
+    def choose_list(self, name):
+        if name == 'susceptible':
+            return self.susceptible
+        elif name == 'exposed':
+            return self.exposed
+        elif name == 'infectious':
+            return self.infectious
+        elif name == 'recovered':
+            return self.recovered
+        else:  # name == 'dead'
+            return self.dead
 
 
 def draw_map(data, i, group):
@@ -47,50 +95,76 @@ def draw_map(data, i, group):
     plt.close(fg)
 
 
+def merge_data_frames(df1, df2, df3):
+    tmp = df1.set_index('name').join(df2.set_index('name'))
+    return tmp.join(df3)
+
+
 def clear_directory(path):
     if os.path.isdir(path):
         for file in os.scandir(path):
             os.remove(file)
 
 
-def merge_data_frames(df1, df2, df3):
-    tmp = df1.set_index('name').join(df2.set_index('name'))
-    return tmp.join(df3)
-
-
-def map_generator(group):             #te instrukcje trzeba będzie przerzucić do main w main.py
-    step = 0
-    data_frame_list = []
-    pandemic_condition = {}    # country/bool pair
-    data_to_map = {'name': [], group: []}
-
-    geopandas_countries = gp.read_file(gp.datasets.get_path('naturalearth_lowres'))
-    csv_countries = pd.read_csv('resources/Country.csv', header=0, sep=';')
-    csv_countries = clear_data(csv_countries)
-
+def load_data(frame_list):
     for file in os.listdir('results/'):
         country = DataFrame('results/'+file)
-        data_frame_list.append(country)
-        data_to_map['name'].append(country.name)
-        data_to_map[group].append(0)
-        pandemic_condition[file] = True
+        frame_list.append(country)
 
-    dead_in_countries = pd.DataFrame(data_to_map, columns=['name', group])
-    dead_in_countries = dead_in_countries.set_index('name')
-    merged = merge_data_frames(geopandas_countries, csv_countries, dead_in_countries)
+
+def gather_global_data(frame_list):
+    dictionary = {'susceptible': [], 'exposed': [], 'infectious': [], 'recovered': [], 'dead': []}
+    step = 0
+    pandemic_condition = {}  # country/bool pair
+
+    for frame in frame_list:
+        pandemic_condition[frame.name] = True
 
     while True in pandemic_condition.values():
-        for frame in data_frame_list:
+        dictionary['susceptible'].append(0)
+        dictionary['exposed'].append(0)
+        dictionary['infectious'].append(0)
+        dictionary['recovered'].append(0)
+        dictionary['dead'].append(0)
+
+        for frame in frame_list:
             if step in range(frame.start_day, frame.end_day+1):
-                dead_in_countries.loc[frame.name][group] = frame.dead_list[step-frame.start_day]
-                merged = merge_data_frames(geopandas_countries, csv_countries, dead_in_countries)
-            if step > frame.end_day:
+                dictionary['susceptible'][step] += frame.susceptible[step-frame.start_day]
+                dictionary['exposed'][step] += frame.exposed[step-frame.start_day]
+                dictionary['infectious'][step] += frame.infectious[step-frame.start_day]
+                dictionary['recovered'][step] += frame.recovered[step-frame.start_day]
+                dictionary['dead'][step] += frame.dead[step-frame.start_day]
+            elif step > frame.end_day:
                 pandemic_condition[frame.name] = False
-            draw_map(merged, step, group)
-            step += 1
+                dictionary['susceptible'][step] += frame.susceptible[frame.end_day]
+                dictionary['exposed'][step] += frame.exposed[frame.end_day]
+                dictionary['infectious'][step] += frame.infectious[frame.end_day]
+                dictionary['recovered'][step] += frame.recovered[frame.end_day]
+                dictionary['dead'][step] += frame.dead[frame.end_day]
+
+        step += 1
+
+    dictionary['susceptible'].pop(len(dictionary['susceptible'])-1)
+    dictionary['exposed'].pop(len(dictionary['exposed'])-1)
+    dictionary['infectious'].pop(len(dictionary['infectious'])-1)
+    dictionary['recovered'].pop(len(dictionary['recovered'])-1)
+    dictionary['dead'].pop(len(dictionary['dead'])-1)
+
+    return dictionary
 
 
 if __name__ == "__main__":
-    clear_directory(os.path.dirname(__file__)+"/maps/")
-    map_generator('exposed')
-    map_generator('dead')
+    data_frame_list = []            #tutaj będą zapisywane dane z plików
+    load_data(data_frame_list)
+
+    mapGen = MapGenerator('resources/Country.csv')
+    mapGen.clear_data()
+    worldStats = gather_global_data(data_frame_list)       #słownik do generowania wykresu dla całego świata
+
+    clear_directory(os.path.dirname(__file__)+"/maps/")         #ta funkcja czyści cały folder, być może będzie potrzebna implementacja kasująca wybrany rodzaj map
+
+    mapGen.generate_maps('susceptible', data_frame_list)
+    mapGen.generate_maps('exposed', data_frame_list)
+    mapGen.generate_maps('infectious', data_frame_list)
+    mapGen.generate_maps('recovered', data_frame_list)
+    mapGen.generate_maps('dead', data_frame_list)
