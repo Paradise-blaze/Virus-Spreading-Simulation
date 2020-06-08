d08026e8f5c4c613d7bb60813449641e4b2bac53
ProxPxD <piotr10tutek@poczta.onet.pl>
2020-06-08 14:33:37 +0200

    additional directories

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-
+import os
 from Window import Window
 
-window = Window("Virus Spreading", "gui.xml")
-window.set_background_path("background.png")
-window.set_map_path("map.png")
+window = Window("Virus Spreading", os.path.join("resources", "gui.xml"))
+window.set_background_path(os.path.join("images", "background.png"))
+window.set_map_path(os.path.join("images", "map.png"))
 size = 580
 window.set_dimensions(int(size * 1.618), size)
 window.open()
